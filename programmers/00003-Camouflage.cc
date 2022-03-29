eba8acaa7f6aeeda4831d3f6f5de497da67726c4
HJJ <wasicrob27@gmail.com>
2022-03-29 13:04:32 +0900

    Add 프로그래머스 위장

--- programmers/00003-Camouflage.cc
+++ programmers/00003-Camouflage.cc
@@ -7,19 +7,14 @@ using namespace std;
 unordered_map<string, int> m;
 vector<int> COUNT_CATEGORY;
 int RESULT;
-void Recursive(int pos, int until, int count, int value) {
+void Recursive(int until, int count, int value) {
     if (until == count) {
-        cout << value << endl;
         RESULT += value;
         return;
     }
-    for (auto i = pos; i != COUNT_CATEGORY.size(); ++i) {
-        const auto& num = COUNT_CATEGORY[i];
-        cout << "until : " << until << " count : " << count << " value : " << value << " num : " << num << endl;
-        Recursive(i + 1, until, count + 1, value);
-        Recursive(i + 1, until, count + 1, value * num);
-        cout << " ------ " << endl;
-    }
+    const auto& num = COUNT_CATEGORY[count];
+    Recursive(until, count + 1, value);
+    Recursive(until, count + 1, value * num);
 }
 int solution(vector<vector<string>> clothes) {
     int answer = 0;
@@ -32,11 +27,9 @@ int solution(vector<vector<string>> clothes) {
     for (auto itr = m.begin(); itr != m.end(); ++itr)
         COUNT_CATEGORY.push_back((*itr).second);
 
-    int sum = 1;
-    for (auto i = 1; i <= m.size(); ++i) {
-        Recursive(0, i, 0, 1);
-        RESULT--;
-    }
+    Recursive(COUNT_CATEGORY.size(), 0, 1);
+    RESULT--;
+
     answer = RESULT;
     cout << answer << endl;
     return answer;
