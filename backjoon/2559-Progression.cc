4617c9c2ec0f9ea76b52599042e7d23738bde58e
JeongHeon <wasicrob27@gmail.com>
2023-01-24 14:26:57 +0900

    Add 백준 수열 2559 V2

--- backjoon/2559-Progression.cc
+++ backjoon/2559-Progression.cc
@@ -4,30 +4,19 @@
 using namespace std;
 
 #define INF INT32_MAX
-int pro[100004];
 int sum[100004];
 int n, k;
-
+int temp;
+int rst = INT32_MIN;
 int main() {
     cin >> n >> k;
-    for (int i = 0; i < n; ++i) cin >> pro[i];
-
-    fill(&sum[0], &sum[0] + 100004, INF);
-    sum[0] = 0;
-    for (int i = 0; i < k; ++i) sum[0] += pro[i];
-    
-    for (int i = 1; i + k - 1 < n; ++i) {
-        sum[i] = sum[i - 1] - pro[i - 1] + pro[i + k - 1];
+    for (int i = 1; i <= n; ++i){
+        cin >> temp;
+        sum[i] = sum[i - 1] + temp;
     }
-
-    // for (int i = 0; i < n; ++i) cout << sum[i] << " ";
-    // cout << "\n";
-
-    int rst = sum[0];
-    for (int i = 0; i < n; ++i) {
-        if(sum[i] == INF) continue;
-        rst = max(rst, sum[i]);
+    for (int i = k; i <= n; i++){
+        rst = max(rst, sum[i] - sum[i - k]);
     }
-    cout << rst;
+    cout << rst << "\n";
     return 0;
 }
