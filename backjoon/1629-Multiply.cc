f6b38abab12b24ca7be50222a102490f26a0cceb
JeongHeon <wasicrob27@gmail.com>
2023-02-05 22:43:07 +0900

    Renew1 백준 곱셈 1629

--- backjoon/1629-Multiply.cc
+++ backjoon/1629-Multiply.cc
@@ -2,50 +2,23 @@
 #include <stack>
 
 using namespace std;
-int a, b, c;
-long long cnt = 1;
-long long one;
-long long two;
+long long a, b, c;
 long long rst;
+long long recursive(long long b2) {
+    if (b2 == 1) return a % c;
+
+    long long ret = recursive(b2 / 2);
+    ret = (ret * ret) % c;
+    if (b2 & 1) ret = (ret * a) % c;
+    return ret;
+}
+
 int main() {
     cin >> a >> b >> c;
 
-    // 1
-    one = 1 * a % c;
-    // 2
-    two = one * a % c;
+    rst = recursive(b);
 
-    if (b == 1) {
-        cout << one;
-        return 0;
-    } else if (b == 2) {
-        cout << two;
-        return 0;
-    }
-
-    stack<int> s;
-    while (b > 0) {
-        s.push(b);
-        b /= 2;
-    }
-
-    cnt = 0;
-    rst = 1;
-    while (!s.empty()) {
-        long long b2 = s.top();
-        // cout << b2 << "\n";
-        s.pop();
-        while (cnt < b2 && cnt != b2) {
-            if (cnt != 0 && cnt * 2 <= b2) {
-                rst = rst * rst % c;
-                cnt *= 2;
-            } else {
-                rst = rst * one % c;
-                cnt += 1;
-            }
-        }
-    }
-    cout << rst;
+    cout << rst << "\n";
 
     return 0;
 }
