8cd1cf6273b62fd9630e36f96b1820dd259ed9bc
JeongHeon <wasicrob27@gmail.com>
2023-02-06 23:25:24 +0900

    Renew1 백준 영역 구하기 2583

--- backjoon/2583-DrawArea.cc
+++ backjoon/2583-DrawArea.cc
@@ -1,73 +1,63 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <tuple>
 #include <algorithm>
 
 using namespace std;
 
-int main() {
-    vector<int> result_all;
-    const int offset_y[4] = {-1, 0, 1, 0};
-    const int offset_x[4] = {0, 1, 0, -1};
-    int m, n, k;
-    cin >> m >> n >> k;
+int visited[102][102];
+int map[102][102];
+vector<int> rst;
+int m, n, k;
+int sy, sx, ex, ey;
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
+constexpr int SQ = -1;
 
-    vector<vector<int>> map(m, vector<int>(n, 0));
+void bfs(int y, int x) {
+    int cnt = 0;
+    queue<pair<int, int>> q;
 
-    for (int kc = 0; kc < k; ++kc) {
-        int start_y, start_x, end_y, end_x;
+    cnt += 1;
+    visited[y][x] = 1;
+    q.push({y, x});
 
-        cin >> start_x >> start_y >> end_x >> end_y;
+    while (!q.empty()) {
+        auto [yy, xx] = q.front();
+        q.pop();
 
-        for (int y = start_y; y < end_y; ++y) {
-            for (int x = start_x; x < end_x; ++x) {
-                map[y][x] = 1;
-            }
+        for (int w = 0; w < 4; ++w) {
+            int ny = yy + dy[w];
+            int nx = xx + dx[w];
+            if (ny < 0 || m <= ny || nx < 0 || n <= nx) continue;
+            if (map[ny][nx] == SQ) continue;
+            if (visited[ny][nx]) continue;
+
+            visited[ny][nx] = 1;
+            q.push({ny, nx});
+            cnt += 1;
         }
     }
+    rst.push_back(cnt);
+}
 
-    int count = 0;
-    vector<vector<bool>> visited(m, vector<bool>(n, false));
+int main() {
+    cin >> m >> n >> k;
+    for (int i = 0; i < k; ++i) {
+        cin >> sx >> sy >> ex >> ey;
+        for (int ii = sy; ii < ey; ++ii)
+            for (int jj = sx; jj < ex; ++jj) map[ii][jj] = SQ;
+    }
+
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {
-            if (map[i][j])
-                continue;
-            else if (visited[i][j])
-                continue;
+            if (visited[i][j] || map[i][j] == SQ) continue;
 
-            count++;
-
-            queue<tuple<int, int, int>> q;
-            q.push(make_tuple(i, j, 1));
-            int maximum = 0;
-            while (!q.empty()) {
-                auto [y, x, c] = q.front();
-                q.pop();
-                if (visited[y][x]) continue;
-
-                visited[y][x] = true;
-                maximum++;
-
-                for (int way = 0; way < 4; way++) {
-                    int next_y = y + offset_y[way];
-                    int next_x = x + offset_x[way];
-
-                    if (next_y < 0 || m <= next_y || next_x < 0 || n <= next_x) continue;
-                    if (map[next_y][next_x] == 1) continue;
-
-                    q.push(make_tuple(next_y, next_x, c + 1));
-                }
-            }
-            result_all.push_back(maximum);
+            bfs(i, j);
         }
     }
-
-    sort(result_all.begin(), result_all.end());
-
-    cout << count << "\n";
-    for (auto v : result_all) cout << v << " ";
-    cout << "\n";
-
+    sort(rst.begin(), rst.end());
+    cout << rst.size() << "\n";
+    for (auto i : rst) cout << i << " ";
     return 0;
 }
