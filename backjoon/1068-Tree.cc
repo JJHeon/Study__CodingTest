18b64b6b75cf86b90017be5c9010de636fb356fb
JeongHeon <wasicrob27@gmail.com>
2023-02-11 18:22:23 +0900

    Renew1 백준

--- backjoon/1068-Tree.cc
+++ backjoon/1068-Tree.cc
@@ -5,42 +5,36 @@ using namespace std;
 
 int n;
 vector<int> node[52];
-int erase[52];
 int input;
 int d;
+int root;
 
-void dfs(int i) {
-    erase[i] = 1;
-    for (auto it : node[i]) dfs(it);
+int dfs(int i) {
+    int ret = 0;
+    int child = 0;
+    for (auto it : node[i]) {
+        if (it == d) continue;
+        ret += dfs(it);
+        child++;
+    }
+    if (child == 0) return 1;
+    return ret;
 }
 
 int main() {
     cin >> n;
     for (int i = 0; i < n; ++i) {
         cin >> input;
-        node[input + 1].push_back(i + 1);
+        if (input == -1)
+            root = i;
+        else
+            node[input].push_back(i);
     }
     cin >> d;
+    if (d == root)
+        cout << 0 << "\n";
+    else
+        cout << dfs(root) << "\n";
 
-    dfs(d + 1);
-
-    int cnt = 0;
-    for (int i = 1; i <= n; ++i) {
-        if (!erase[i]) {
-            if (node[i].size() == 0) {
-                cnt++;
-                continue;
-            }
-            bool flag = true;
-            for (auto it : node[i]) {
-                if (!erase[it]) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) cnt++;
-        }
-    }
-    cout << cnt << "\n";
     return 0;
 }
