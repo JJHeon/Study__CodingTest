cf571725c432324774cb8fbff59eb312256040de
JeongHeon <wasicrob27@gmail.com>
2023-02-13 20:35:36 +0900

    Renew1 백준 17298 오큰수

--- backjoon/17298-FiveBigNumber.cc
+++ backjoon/17298-FiveBigNumber.cc
@@ -1,39 +1,27 @@
 #include <iostream>
+#include <cstring>
+#include <stack>
 
 using namespace std;
 
 int n;
 int arr[1000002];
 int rst[1000002];
-int mx = 0;
-
-int f(int i, int v) {
-    for (int j = i + 1; j <= n; ++j) {
-        if (rst[j] != -1 && v < rst[j]) return rst[j];
-    }
-    return -1;
-}
+stack<int> stk;
 
 int main() {
     cin >> n;
-    for (int i = 1; i <= n; ++i) cin >> arr[i];
-
-    rst[n] = -1;
-    mx = arr[n];
-
-    for (int i = n - 1; i > 0; i--) {
-        if (arr[i] < arr[i + 1])
-            rst[i] = arr[i + 1];
-        else if (arr[i] < mx) {
-            rst[i] = f(i, arr[i]);
-            continue;
-        } else
-            rst[i] = -1;
-
-        if (arr[i] > mx) mx = arr[i];
+    memset(rst, -1, sizeof(rst));
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+        while (stk.size() && arr[stk.top()] < arr[i]) {
+            rst[stk.top()] = arr[i];
+            stk.pop();
+        }
+        stk.push(i);
     }
 
-    for (int i = 1; i <= n; ++i) cout << rst[i] << " ";
+    for (int i = 0; i < n; ++i) cout << rst[i] << " ";
 
     return 0;
 }
