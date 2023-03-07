62c3cd07169937bbe614a5e5484c57399e9d025f
JeongHeon <wasicrob27@gmail.com>
2023-03-07 22:27:42 +0900

    Renew2 백준 1931 회의실 배정

--- backjoon/1931-AssignMeetingRoom.cc
+++ backjoon/1931-AssignMeetingRoom.cc
@@ -7,10 +7,8 @@
 using namespace std;
 
 int n;
-
 vector<pair<int, int>> v;
-
-vector<pair<int, int>> ans;
+int rst = 1;
 
 int main() {
     cin >> n;
@@ -19,22 +17,21 @@ int main() {
 
     for (int i = 0; i < n; ++i) {
         cin >> st >> et;
-        v.push_back({st, et});
+        v.push_back({et, st});
     }
 
     sort(v.begin(), v.end());
 
-    ans.push_back(v[0]);
-
-    for (int i = 1; i < v.size(); ++i) {
-        if (ans[ans.size() - 1].second > v[i].second) {
-            ans.pop_back();
-            ans.push_back(v[i]);
-        } else if (ans[ans.size() - 1].second <= v[i].first)
-            ans.push_back(v[i]);
+    int from = v[0].second;
+    int to = v[0].first;
+    for (int i = 1; i < n; ++i) {
+        if (v[i].second < to) continue;
+        from = v[i].second;
+        to = v[i].first;
+        rst++;
     }
 
-    cout << ans.size();
+    cout << rst;
 
     return 0;
 }
