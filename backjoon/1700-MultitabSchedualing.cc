7f5cc11a2d136fd9853dd5f1ec55876acffe4092
JeongHeon <wasicrob27@gmail.com>
2023-03-09 20:22:39 +0900

    Renew1 백준 1700 멀티탭 스케줄링

--- backjoon/1700-MultitabSchedualing.cc
+++ backjoon/1700-MultitabSchedualing.cc
@@ -1,48 +1,45 @@
 #include <iostream>
-#include <set>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
 
 using namespace std;
 
 int rst, n, k;
 int mp[102];
-set<int> st;
+int visit[102];
+vector<int> v;
 
 int main() {
-    cin >> n >> k;
-    for (int i = 0; i < k; ++i) cin >> mp[i];
+    cin >> k >> n;
+    for (int i = 0; i < n; ++i) cin >> mp[i];
 
-    for (int i = 0; i < k;) {
-        if (st.size() < n) {
-            st.insert(mp[i++]);
-        }
-
-        else if (st.find(mp[i]) == st.end()) {
-            int t = -1;
-            int mm = 0;
-
-            for (auto it : st) {
-                bool is_end = false;
-
-                for (int j = i; j < k; ++j) {
-                    if (it == mp[j]) {
-                        is_end = true;
-                        if (mm < j - i) {
-                            t = it;
-                            mm = j - i;
+    for (int i = 0; i < n; ++i) {
+        if (!visit[mp[i]]) {
+            if (v.size() == k) {
+                int idx = 0, pos;
+                for (auto it : v) {
+                    int pick = INT32_MAX;
+                    for (int j = i + 1; j < n; ++j) {
+                        if (it == mp[j]) {
+                            pick = j;
+                            break;
                         }
-                        break;
+                    }
+                    if (idx < pick) {
+                        idx = pick;
+                        pos = it;
                     }
                 }
-                if (!is_end) {
-                    t = it;
-                    break;
-                }
+                visit[pos] = 0;
+                rst++;
+                v.erase(find(v.begin(), v.end(), pos));
             }
-            st.erase(t);
-            rst++;
-        } else
-            i++;
+            v.push_back(mp[i]);
+            visit[mp[i]] = 1;
+        }
     }
+
     cout << rst;
     return 0;
 }
