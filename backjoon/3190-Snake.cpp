f9d309f17378c5248d0780dd27b9324f68475e68
JeongHeon <wasicrob27@gmail.com>
2023-03-16 23:04:53 +0900

    Renew1 백준 3190 뱀

--- backjoon/3190-Snake.cpp
+++ backjoon/3190-Snake.cpp
@@ -1,192 +1,95 @@
-/*
-@Title		: 백준
-@Name		: 뱀
-@First0		: 20.10.22
-@Revision1	: 21.01.06
-@ETC		: G5
-항상 Top이 최신 코드
-*/
-
-#define	__BACKJOON_3190__	1		//21.01.06	//뱀
-
-#if __BACKJOON_3190__
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <algorithm>
+#include <iostream>
+
+#include <vector>
+
+#include <deque>
 
 using namespace std;
 
-int map[101][101];
-int N, K, L;
+int n, k, w, l, y, x, t, aft, vt;
 
-int SEC;
+char dir;
 
-queue<pair<int, int>> snake;
-vector<pair<int, char>> dirct_list;
+int mp[102][102];
 
-int pin_list;
-int current_dirct = 0;
+int dy[4] = {-1, 0, 1, 0};
 
-int x_way[4] = { 0,-1,0,1 };
-int y_way[4] = { 1, 0, -1,0 };
+int dx[4] = {0, 1, 0, -1};
 
-int pos_x = 1;
-int pos_y = 1;
+deque<pair<int, int>> dq;
 
+vector<pair<int, int>> v;
 
+int main() {
+    cin >> n >> k;
+    for (int i = 0; i < k; ++i) {
+        cin >> y >> x;
+        mp[y - 1][x - 1] = -1;
+    }
 
-int main(int argc, char** argv) {
+    cin >> l;
+    for (int i = 0; i < l; ++i) {
+        cin >> aft >> dir;
+        v.push_back({aft, dir});
+    }
 
-	cin >> N;
-	cin >> K;
+    w = 1;
+    y = 0;
+    x = 0;
+    dq.push_back({y, x});
+    mp[0][0] = 1;
 
-	int tmp_x, tmp_y;
-	for (int i = 0; i < K; i++) {
-		cin >> tmp_x >> tmp_y;
-		map[tmp_x][tmp_y] = 1;
-	}
+    while (true) {
+        t++;
 
-	cin >> L;
-	int tmp2_s;
-	char tmp2_d;
-	for (int i = 0; i < L; i++) {
-		cin >> tmp2_s >> tmp2_d;
-		dirct_list.push_back(make_pair(tmp2_s, tmp2_d));
-	}
-	// --
+        y = dq.back().first;
+        x = dq.back().second;
 
-	snake.push(make_pair(1, 1));
-	// --
+        int ny = y + dy[w];
+        int nx = x + dx[w];
 
-	while (true) {
-		SEC++;
+        if (ny < 0 || n <= ny || nx < 0 || n <= nx) break;
+        if (mp[ny][nx] == 1) break;
 
-		pos_x = snake.back().first;
-		pos_y = snake.back().second;
+        dq.push_back({ny, nx});
 
-		pos_x += x_way[current_dirct];
-		pos_y += y_way[current_dirct];
+        if (mp[ny][nx] == -1)
+            mp[ny][nx] = 1;
+        else {
+            mp[ny][nx] = 1;
+            auto [i, j] = dq.front();
+            mp[i][j] = 0;
+            dq.pop_front();
+        }
 
-		if (pos_x<1 || pos_x>N || pos_y < 1 || pos_y > N) break;
-		if (map[pos_x][pos_y] == 2) break;
+        if (v.size() > vt) {
+            aft = v[vt].first;
+            dir = v[vt].second;
+            if (aft == t) {
+                switch (dir) {
+                    case 'D':
+                        w = (w + 1) % 4;
+                        break;
 
-		snake.push(make_pair(pos_x, pos_y));
+                    case 'L':
+                        w = (w + 3) % 4;
+                        break;
+                }
+                vt++;
+            }
+        }
 
-		if (map[pos_x][pos_y] != 1) {
-			pair<int, int> del_pos = snake.front();
-			map[del_pos.first][del_pos.second] = 0;
-			snake.pop();
-		}
-		map[pos_x][pos_y] = 2;
+        // cout << t << "\n";
+        // for(int i = 0; i < n; i++) {
+        // 	for(int j = 0; j < n; ++j) cout << mp[i][j] << " ";
+        // 	cout << "\n";
+        // }
+        // cout <<"\n\n";
+    }
+    cout << t;
 
-		if (dirct_list.size() > pin_list) {
-			if (dirct_list[pin_list].first == SEC) {
-				switch (dirct_list[pin_list].second) {
-				case 'L':
-					current_dirct = (current_dirct + 1) % 4;
-					break;
-				case 'D':
-					current_dirct = (current_dirct + 3) % 4;
-					break;
-				}
-
-				pin_list++;
-			}
-		}
-
-	}
-	cout << SEC;
+    return 0;
 }
-
-//0 - First
-#if 0
-#include <iostream>
-#include <vector>
-#include <queue>
-
-using namespace std;
-
-int map[101][101];
-int N, K, L;
-
-int SEC;
-
-queue<pair<int, int>> snake;
-vector<pair<int, char>> dirct_list;
-
-int pin_list;
-int current_dirct = 0;
-
-int x_way[4] = { 0,-1,0,1 };
-int y_way[4] = { 1, 0, -1,0 };
-
-int pos_x = 1;
-int pos_y = 1;
-
-
-
-int main(int argc, char** argv) {
-
-	cin >> N;
-	cin >> K;
-
-	int tmp_x, tmp_y;
-	for (int i = 0; i < K; i++) {
-		cin >> tmp_x >> tmp_y;
-		map[tmp_x][tmp_y] = 1;
-	}
-
-	cin >> L;
-	int tmp2_s;
-	char tmp2_d;
-	for (int i = 0; i < L; i++) {
-		cin >> tmp2_s >> tmp2_d;
-		dirct_list.push_back(make_pair(tmp2_s, tmp2_d));
-	}
-	// --
-
-	snake.push(make_pair(1, 1));
-	// --
-
-	while (true) {
-		SEC++;
-
-		pos_x = snake.back().first;
-		pos_y = snake.back().second;
-
-		pos_x += x_way[current_dirct];
-		pos_y += y_way[current_dirct];
-
-		if (pos_x<1 || pos_x>N || pos_y < 1 || pos_y > N) break;
-		if (map[pos_x][pos_y] == 2) break;
-
-		snake.push(make_pair(pos_x, pos_y));
-
-		if (map[pos_x][pos_y] != 1) {
-			pair<int, int> del_pos = snake.front();
-			map[del_pos.first][del_pos.second] = 0;
-			snake.pop();
-		}
-		map[pos_x][pos_y] = 2;
-
-		if (dirct_list.size() > pin_list) {
-			if (dirct_list[pin_list].first == SEC) {
-				switch (dirct_list[pin_list].second) {
-				case 'L':
-					current_dirct = (current_dirct + 1) % 4;
-					break;
-				case 'D':
-					current_dirct = (current_dirct + 3) % 4;
-					break;
-				}
-
-				pin_list++;
-			}
-		}
-
-	}
-	cout << SEC;
-}
-#endif
-
-#endif
