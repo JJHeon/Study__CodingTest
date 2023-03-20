e06c02ea562c9fd82573c8ef2b88f2006cb2d2f5
JeongHeon <wasicrob27@gmail.com>
2023-03-20 20:09:54 +0900

    Renew1 백준 1912 연속합 (못품)

--- backjoon/1912-ContinuousSum.cc
+++ backjoon/1912-ContinuousSum.cc
@@ -1,36 +1,25 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <cstdint>
+#include <cstring>
 
 using namespace std;
 
-int RES = INT32_MIN;
-int N;
-int GET_INPUT[100001];
-int MAP[100001];
-
-int Solution() {
-    // Input, // Write input process using std::cin
-    cin >> N;
-    for (int i = 1; i <= N; ++i) cin >> MAP[i];
-
-    // Process
-
-    if (MAP[1] > RES) RES = MAP[1];
-
-    for (int i = 2; i <= N; ++i) {
-        if (MAP[i - 1] > 0 && MAP[i - 1] + MAP[i] > 0) MAP[i] = MAP[i - 1] + MAP[i];
-        if (MAP[i] > RES) RES = MAP[i];
-    }
-
-    // Result
-    cout << RES;
-    return 0;
-}
+int n;
+int a[100004];
+int rst = INT32_MIN;
 
 int main() {
-    Solution();
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        cin >> a[i];
+    }
 
+    if (rst < a[1]) rst = a[1];
+    for (int i = 2; i <= n; ++i) {
+        if (a[i - 1] > 0 && a[i - 1] + a[i] > 0) a[i] = a[i - 1] + a[i];
+        if (rst < a[i]) rst = a[i];
+    }
+
+    cout << rst;
     return 0;
 }
